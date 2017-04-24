307bdae30ec0b666c7ecb2a7a6c4f6ab2d649f66
hogehoge1298 <hogehoge1298@gmail.com>
2017-04-24 17:44:33 +0900

    更新処理と入力処理の実装

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,3 +1,43 @@
 window.onload = function(){
     console.log("hello, world!");
+    init();
+}
+
+var scene;  //シーンオブジェクト
+//初期化
+function init(){
+    //シーンオブジェクトの取得
+    scene = document.querySelector('a-scene');
+    //入力イベントの実装
+    document.addEventListener("keydown", keyInput);
+
+    //更新処理の開始
+    //update();
+}
+
+//更新
+function update(){
+    setTimeout(update, 1000);
+}
+
+//a-boxを生成する
+function generateBox(){
+    var el = document.createElement('a-entity');
+    var randX = Math.floor( Math.random() * 11 ) ;
+    var randY = Math.floor( Math.random() * 11 ) ;
+
+    el.setAttribute('geometry',{
+        primitive: 'box',
+        height: 3,
+        width: 1,
+    });
+
+    scene.appendChild(el);
+}
+
+//入力イベント
+function keyInput(e)
+{
+    console.log(e.keyCode);
+    generateBox();
 }
